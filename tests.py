508b7eeac86868fe541e5b9f12d62d269fc93a87
brinda gurusamy <brinda_gurusamy@berkeley.edu>
2021-04-27 00:52:43 -0700

    Adding  pytest Module

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-from hello import app
-with app.test_client() as c:
-    response = c.get('/')
-    assert response.data == b'Hello World!'
-    assert response.status_code == 200
+import pytest
+from .hello import func
+
+def test_answer():
+    assert func(4) == 5
