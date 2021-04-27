1c92a99e6f906fadcc5d460be9202e81d7ea6c1b
brinda gurusamy <brinda_gurusamy@berkeley.edu>
2021-04-27 01:30:20 -0700

    Language Similarity Check Function

---
+++ mentor.py
@@ -0,0 +1,15 @@
+# Understands How Similar Mentors are to Mentees
+class Mentor:
+
+    def __init__(self, name, language, interests, country):
+        self.name = name
+        self.language = language
+        self.interests = interests
+        self.country = country
+    
+    def language_similarity(self, language):
+        if self.language == language:
+            return 1
+        return 0
+
+    
--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
 import pytest
-from .hello import func
+import sys
+sys.path.append(".")
+from mentor import Mentor
 
-def test_answer():
-    assert func(4) == 5
+mentorA = Mentor("Jack", "English", "Computer Science", "United States")
+print(mentorA)
+
+def test_mentorAMenteeBSpeaksEngHasSimilarityScoreOne():
+    mentorA = Mentor("Jack", "English", "Computer Science", "United States")
+    assert 1 == mentorA.language_similarity("English")
