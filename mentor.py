fd85ad7f065a5096e9c7b8ca258f6c1baf2fd687
brinda gurusamy <brinda_gurusamy@berkeley.edu>
2021-04-29 19:52:34 -0700

    Converted all similarity calculations to use Enum Values

--- mentor.py
+++ mentor.py
@@ -8,21 +8,45 @@ class Mentor:
     def __init__(self, name, language, interests, country):
         try:
             self.name = name
-            self.language = Language(language)
-            self.interests = Interest(interests)
-            self.country = Country(country)
+            self.language = self.convertToEnum(language, Language)
+            self.interests = self.convertToEnum(interests, Interest)
+            self.country = self.convertToEnum(country, Country)
         except AttributeError as e:
             print(e)
-    
+
+    def convertToEnum(self, values, enumType):
+        values = values.split(",")
+        result = set()
+        for value in values:
+            value = value.strip()
+            result.add(enumType(value))
+        return result
+
     def language_similarity(self, language):
-        if self.language == Language(language):
-            return 1
-        return 0
+        values = language.split(",")
+        total, matched = 0, 0
+        for value in values:
+            value = value.strip()
+            if Language(value) in self.language:
+                matched+=1
+            total+=1
+        return round(matched/total, 2)
 
     def location_similarity(self, location):
         if self.country == Country(location):
-            return 1
-        return 0
+            return 1.0
+        return 0.0
+
+    def interests_similarity(self, interests):
+        values = interests.split(",")
+        total, matched = 0, 0
+        for value in values:
+            value = value.strip()
+            if Interest(value) in self.interests:
+                matched+=1
+            total+=1
+        return round(matched/total, 2)
+
 
 
 
--- tests.py
+++ tests.py
@@ -7,14 +7,24 @@ from constants import Language, Country, Interest
 mentorA = Mentor("Jack", "English", "Computer Science", "United States")
 
 def test_mentorAMenteeBSpeaksEngHasSimilarityScoreOne():
-    assert 1 == mentorA.language_similarity("English")
+    assert 1.0 == mentorA.language_similarity("English")
 
 def test_mentorLocationUSMenteeLocationUKShouldReturnZero():
-    assert 0 == mentorA.location_similarity("United Kingdom")
+    assert 0.0 == mentorA.location_similarity("United Kingdom")
 
 def test_wrongEnumForCountryPolandRaisesError():
     with pytest.raises(ValueError):
         mentorB = Mentor("B", "English", "Computer Science", "Poland")
+
+def test_MenteeCSDSMLMentorCSMLShouldReturnSimilarityZeroPointSevenFive():
+
+    mentorB = Mentor("B", "English", "Computer Science, Data Science", "Germany")
+    assert 0.67 == mentorB.interests_similarity("Computer Science, Data Science, Machine Learning")
+
+
+
+    
+
 
 
 
